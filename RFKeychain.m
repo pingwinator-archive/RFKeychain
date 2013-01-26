b9c96743c3a852f4bd0d7e99e0a04b0cec612570
Tim Brückmann <tim@tibr.me>
2013-01-26 10:34:19 +0100

    Add iOS compatibility

--- RFKeychain.m
+++ RFKeychain.m
@@ -15,35 +15,35 @@
             account:(NSString *)account
             service:(NSString *)service
 {
-    const char *serviceChars = [service UTF8String];
-    const char *accountChars = [account UTF8String];
-    const char *passwordChars = [password UTF8String];
+    NSString *existingPassword = [self passwordForAccount:account service:service];
     
-    SecKeychainItemRef existingItemRef;
-    OSStatus status = SecKeychainFindGenericPassword(NULL,
-                                                     (UInt32)strlen(serviceChars),
-                                                     serviceChars,
-                                                     (UInt32)strlen(accountChars),
-                                                     accountChars,
-                                                     NULL,
-                                                     NULL,
-                                                     &existingItemRef);
+    OSStatus status = noErr;
     
-    if (status != errKCItemNotFound) {
-        status = SecKeychainItemModifyAttributesAndData(existingItemRef,
-                                                        NULL,
-                                                        (UInt32)strlen(passwordChars),
-                                                        passwordChars);
-        CFRelease(existingItemRef);
+    if (existingPassword) {
+        if ([existingPassword isEqualToString:password] == NO) {
+            CFMutableDictionaryRef query = [self createDictionaryForService:service account:account];
+            NSData *passwordData = [password dataUsingEncoding:NSUTF8StringEncoding];
+            const void *keys[] = { kSecValueData };
+            const void *values[] = { (__bridge CFDataRef)passwordData };
+            CFDictionaryRef attributesToUpdate = CFDictionaryCreate(NULL,
+                                                                    keys,
+                                                                    values,
+                                                                    sizeof(keys) / sizeof(*keys),
+                                                                    &kCFTypeDictionaryKeyCallBacks,
+                                                                    &kCFTypeDictionaryValueCallBacks);
+
+            status = SecItemUpdate(query, attributesToUpdate);
+            
+            CFRelease(query);
+		}
     } else {
-        status = SecKeychainAddGenericPassword(NULL,
-                                               (UInt32)strlen(serviceChars),
-                                               serviceChars,
-                                               (UInt32)strlen(accountChars),
-                                               accountChars,
-                                               (UInt32)strlen(passwordChars),
-                                               passwordChars,
-                                               NULL);
+        NSData *passwordData = [password dataUsingEncoding:NSUTF8StringEncoding];
+        CFMutableDictionaryRef query = [self createDictionaryForService:service account:account];
+        CFDictionarySetValue(query, kSecValueData, (__bridge CFDataRef)passwordData);
+        
+        status = SecItemAdd(query, NULL);
+        
+        CFRelease(query);
     }
     
     return status == noErr;
@@ -52,47 +52,49 @@
 + (NSString *)passwordForAccount:(NSString *)account
                          service:(NSString *)service
 {
-    const char *accountChars = [account UTF8String];
-    const char *serviceChars = [service UTF8String];
+    CFMutableDictionaryRef query = [self createDictionaryForService:service account:account];
+    CFDictionarySetValue(query, kSecReturnData, kCFBooleanTrue);
     
-    UInt32 passwordLength;
-    char *password;
+    CFTypeRef result;
+    OSStatus status = SecItemCopyMatching(query, &result);
     
-    OSStatus status = SecKeychainFindGenericPassword(NULL,
-                                                     (UInt32)strlen(serviceChars),
-                                                     serviceChars,
-                                                     (UInt32)strlen(accountChars),
-                                                     accountChars,
-                                                     &passwordLength,
-                                                     (void **)&password,
-                                                     NULL);
+    CFRelease(query);
     
     if (status != noErr) {
         return nil;
     }
     
-    NSData *passwordData = [NSData dataWithBytes:password length:passwordLength];
-    NSString *passwordString = [[NSString alloc] initWithData:passwordData encoding:NSUTF8StringEncoding];
-    SecKeychainItemFreeContent(NULL, password);
- 
-    return passwordString;
+    NSData *resultData = (__bridge_transfer NSData *)result;
+    NSString *resultString = [[NSString alloc] initWithData:resultData encoding:NSUTF8StringEncoding];
+    return resultString;
 }
 
 + (BOOL)deletePasswordForAccount:(NSString *)account
                          service:(NSString *)service
-{   
-    NSDictionary *query = (@{
-                           (__bridge id)kSecClass : (__bridge id)kSecClassGenericPassword,
-                           (__bridge id)kSecAttrService : service,
-                           (__bridge id)kSecAttrAccount : account
-                           });
+{
+    CFMutableDictionaryRef query = [self createDictionaryForService:service account:account];
     
-    OSStatus status = SecItemDelete((__bridge CFDictionaryRef)query);
+    OSStatus status = SecItemDelete(query);
+    
+    CFRelease(query);
     
     return (
             status == noErr
-            || status == errKCItemNotFound
+            || status == errSecItemNotFound
             );
+}
+
++ (CFMutableDictionaryRef)createDictionaryForService:(NSString *)service
+                                             account:(NSString *)account
+{
+    CFMutableDictionaryRef query = CFDictionaryCreateMutable(NULL,
+                                                             0,
+                                                             &kCFTypeDictionaryKeyCallBacks,
+                                                             &kCFTypeDictionaryValueCallBacks);
+    CFDictionarySetValue(query, kSecClass, kSecClassGenericPassword);
+    CFDictionarySetValue(query, kSecAttrService, (__bridge CFStringRef)service);
+    CFDictionarySetValue(query, kSecAttrAccount, (__bridge CFStringRef)account);
+    return query;
 }
 
 @end
